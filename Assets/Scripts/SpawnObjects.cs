62190c11dabb927c84922368e7a2c585d188156e
Barney Nicholls <barneynicholls@Headforwardss-iMac-3.local>
2019-11-06 14:21:18 +0000

    spawn x y z

--- Assets/Scripts/SpawnObjects.cs
+++ Assets/Scripts/SpawnObjects.cs
@@ -2,8 +2,39 @@
 
 public class SpawnObjects : MonoBehaviour
 {
+    public int XCount = 10;
+
+    public int YCount = 10;
+
+    public int ZCount = 10;
+
+    public float Spacing = 1.2f;
+
     void Start()
     {
-        Instantiate(Resources.Load("Cube"), gameObject.transform);
+        Spawn();
+    }
+
+    public void Spawn()
+    {
+
+        for (int y = 0; y < YCount; y++)
+        {
+            for (int x = 0; x < XCount; x++)
+            {
+              for (int z = 0; z < ZCount; z++)
+              {
+                var pos = gameObject.transform.position;
+
+                pos.x += Spacing * x;
+                pos.y += Spacing * y;
+                pos.z += Spacing * z;
+
+                Debug.Log(string.Format("x:{0}, y:{1}, z:{2}", pos.x, pos.y, pos.z));
+
+                Instantiate(Resources.Load("Cube"), pos, Quaternion.identity);
+              }
+            }
+        }
     }
 }
